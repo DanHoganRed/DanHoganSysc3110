18ab7016b2c02c86048b2b5792bbb6cb7bdc7888
DanHoganRed <irishvking@hotmail.com>
2015-11-19 13:08:32 -0500

    Lab7

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,37 +1,66 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
-import javax.swing.*;  
+import javax.swing.*;
 
 public class AddressBook {
-	
-	ArrayList<BuddyInfo> Book = new ArrayList<BuddyInfo>();
-	
+
+	private DefaultListModel<BuddyInfo> Book = new DefaultListModel<BuddyInfo>();
+
 	public static void main(String[] args) {
-		GUI G = new GUI();
+		//GUI G = new GUI();
+		
+	}
+
+	public void addBuddy(BuddyInfo Buddy) {
+		this.Book.add(Book.getSize(), Buddy);
+	}
+
+	public void removeBuddy(BuddyInfo Buddy) {
+		this.Book.removeElement(Buddy);
+	}
+
+	public DefaultListModel<BuddyInfo> getAddressBook() {
+		return Book;
 	}
 	
-	public void addBuddy(BuddyInfo Buddy){
-		this.Book.add(Buddy);
+	public int size() {
+		return Book.size();
 	}
 	
-	public void removeBuddy(BuddyInfo Buddy){
-		this.Book.remove(Buddy);
+	public void clear() {
+		Book.removeAllElements();
 	}
 	
-	public String listToString()
+	public void export() throws IOException
 	{
+		String s = "";
+		Object[] n = Book.toArray();
+		BufferedWriter out = new BufferedWriter(new FileWriter("myFile.txt"));
+		for (int i = 0; i < Book.size(); i++) {
+			s = ((BuddyInfo) n[i]).toString() + "\n";
+			out.write(s);
+		}
+		out.close();
+	}
+
+
+	public String listToString() {
 		String s = "Friends: \n";
-		for(BuddyInfo B:Book){
+		Object[] n = Book.toArray();
+		for (int i = 0; i < Book.size(); i++) {
 			s += "Name: ";
-			s += B.getName();
+			s += ((BuddyInfo) n[i]).getName();
 			s += "\nAge: ";
-			s += B.getAge();
+			s += ((BuddyInfo) n[i]).getAge();
 			s += "\nPhone Number: ";
-			s += B.getPhonenumber();
+			s += ((BuddyInfo) n[i]).getPhonenumber();
 			s += "\n";
 		}
-		return s;	
+		return s;
 	}
 }
